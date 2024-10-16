3c5dccf1739a366719a7e22cfb4703837395fe82
Aristide Proriol <aristidepr@gmail.com>
2024-10-16 17:33:43 +0200

    Renomme le fichier main.jsx en index.jsx pour plus de clarté

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes.jsx'
 import './i18n.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

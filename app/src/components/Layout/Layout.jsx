cafc5d963e8ac1e049b31c96e45e5d000dc6b146
Aristide Proriol <aristidepr@gmail.com>
2024-10-17 14:39:56 +0200

    Ajoute le module de changement de thème (sombre/clair)

--- app/src/components/Layout/Layout.jsx
+++ app/src/components/Layout/Layout.jsx
@@ -5,6 +5,7 @@ import { Link, Outlet } from "react-router-dom";
 const Layout = () => {
     const { t, i18n: { changeLanguage, language }} = useTranslation();
     const [currentLanguage, setCurrentLanguage] = useState(language)
+    const [isDark, setIsDark] = useState(false);
     
     const handleChangeLanguage = () => {
         const newLanguage = currentLanguage === "fr" ? "en" : "fr";
@@ -12,10 +13,25 @@ const Layout = () => {
         changeLanguage(newLanguage);
     }
 
+    const handleChangeColorScheme = () => {
+        if (isDark) {
+            setIsDark(false);
+            document.body.classList.remove('dark');
+        }
+        else {
+            setIsDark(true);
+            document.body.classList.add('dark');
+        }
+    }
+
     return (
         <>
             <h1>Targetle</h1>
             <Link onClick={handleChangeLanguage}>{t("layout.changeLangage")}</Link>
+            <br/>
+            <Link onClick={handleChangeColorScheme}>
+                {isDark ? t("layout.switchToLightMode") : t("layout.switchToDarkMode")}
+            </Link>
             <Outlet />
         </>
     )

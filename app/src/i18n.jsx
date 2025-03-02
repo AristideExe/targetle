fc97b63b3bbe3c1227208ac39be33a893e4c20e5
AristideExe <aristidepr@gmail.com>
2025-03-02 19:42:58 +0100

    Utilise la langue du navigateur du joueur par défaut

--- app/src/i18n.jsx
+++ app/src/i18n.jsx
@@ -1,16 +1,18 @@
 import i18n from "i18next"
+import detector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next"
 import fr from './locales/fr.json'
 import en from './locales/en.json'
 
 i18n
+    .use(detector)
     .use(initReactI18next)
     .init({
         resources: {
             fr: {...fr},
             en: {...en}
         },
-        fallbackLng: "fr",
+        fallbackLng: "en",
         interpolation: {
             escapeValue: false,
           }
